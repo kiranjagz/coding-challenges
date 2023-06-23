4daf4792b613af445284f354a657f7884835e548
Kiran <kiranjagz@gmai.com>
2023-06-23 19:07:07 +0200

    feat: alternate implementation

---
+++ src/Repeat.Word.Game/AlternateImplementation.cs
@@ -0,0 +1,53 @@
+﻿namespace ChatGPT.Game.Repeat.Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter text to be analyzed!");
+
+                    var sentence = Console.ReadLine();
+
+                    while (string.IsNullOrEmpty(sentence))
+                    {
+                        Console.WriteLine("Not a valid sentence, try again please.");
+                        sentence = Console.ReadLine();
+                    }
+
+                    string[] words = sentence.ToUpper().Split(' ');
+
+                    HashSet<string> wordSet = new HashSet<string>();
+                    string? repeatedWord = null;
+
+                    foreach (string word in words)
+                    {
+                        if (!wordSet.Add(word))
+                        {
+                            repeatedWord = word;
+                            break;
+                        }
+                    }
+
+                    if (repeatedWord != null)
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error has occurred, error message: {ex.Message}");
+                }
+
+                Console.Read();
+            }
+        }
+    }
+}
