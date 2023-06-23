31f21520b3a87160c3ca50690beaefac73d7b619
Kiran <kiranjagz@gmai.com>
2023-06-23 19:10:44 +0200

    feat: added simple array example

---
+++ src/Simple.Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Datastructure
+{
+    internal class FindHighestValue
+    {
+        public int FindTheLargestValue(int[] arr)
+        {
+            var length = arr.Length;
+            var max = arr[0];
+
+            for (int i = 1; i < length; i++)
+            {
+                var temp = arr[i];
+
+                if (temp > max)
+                {
+                    max = temp;
+                }
+            }
+
+            return max;
+        }
+    }
+}
