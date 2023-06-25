cfb48766e9a8ee0ecb2fa4fd093d2a57b38c65c3
Kiran <kiranjagz@gmai.com>
2023-06-25 21:16:59 +0200

    feat: OOP additions

---
+++ src/Async/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Outside the loop");
+
+int numberOfTimes = 5; // Number of times to display "Hello, World!"
+
+await Task.Run(() =>
+{
+    for (int i = 0; i < numberOfTimes; i++)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+});
+
+Console.WriteLine("Print something else after the loop");
---
+++ src/Heap.Example/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Heap.Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hasValue = new HasValue();
+            hasValue.Value = 1;
+            var hash = hasValue.GetHashCode();
+            Console.WriteLine(hash);
+            Console.WriteLine(hasValue.Value);
+
+            hasValue.Value = 2;
+            ChangeValue(hasValue);
+
+            Console.WriteLine(hasValue.Value);
+            Console.ReadLine();
+        }
+
+        public static void ChangeValue(HasValue hasValue)
+        {
+            var hash = hasValue.GetHashCode();
+            Console.WriteLine(hash);
+
+            hasValue = new HasValue();
+
+            hasValue.Value = 3;
+            var hash1 = hasValue.GetHashCode();
+            Console.WriteLine(hash1);
+            Console.WriteLine(hasValue.Value);
+        }
+
+        public class HasValue
+        {
+            public int Value;
+        }
+    }
+}
