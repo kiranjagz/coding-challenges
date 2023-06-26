ae8905e5080b55139684e47ba34515a11c11b71d
Kiran <kiranjagz@gmai.com>
2023-06-26 19:04:39 +0200

    feat: added sum of even numbers

---
+++ src/EvenSumOfNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EvenSumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 8, 11, 14 };
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+                else
+                    oddSum = oddSum + numbers[i];
+            }
+
+            Console.WriteLine("Even sum: " + evenSum);
+
+            Console.WriteLine("Odd sum: " + oddSum);
+        }
+    }
+}
